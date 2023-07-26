8c3b77c5aa5beb896b1cc5830d592c3e4b7620c1
Pere Lokesh <90858590+perelokesh@users.noreply.github.com>
2023-07-26 22:58:31 +0530

    added chatengine to server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({origin: true}));
+
+
+app.post('/auth', async () => {
+  const {username} = req.body;
+  try{
+    const r = await axios.put( 
+      "https://api.chatengine.io/users/",
+    { username: username, secret: username, first_name: username },
+    { headers: { "Private-Key": process.env.Key } });
+    return res.status(r.status).json(r.data);
+  } 
+ catch (e) {
+  return res.status(e.response.status).json(e.response.data);
+}
+})
+app.listen(process.env.Port, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
